d844b843133ab124f6e29c12a05907d19faf4d04
Ihoronya <kalmyk38@gmail.com>
2023-06-26 12:39:51 +0300

    HW no. 7

---
+++ js/01-gallery.js
@@ -0,0 +1,54 @@
+import { galleryItems } from "./gallery-items.js";
+// Change code below this line
+
+const gallery = document.querySelector(".gallery");
+
+gallery.addEventListener("click", onGalleryClick);
+
+const markup = galleryItems
+  .map(
+    ({ preview, original, description }) =>
+      `<li class="gallery__item">
+  <a class="gallery__link" href="${original}">
+    <img
+      class="gallery__image"
+      src="${preview}"
+      data-source="${original}"
+      alt="${description}"
+    />
+  </a>
+  </li>`
+  )
+  .join("");
+
+gallery.insertAdjacentHTML("beforeend", markup);
+
+function onGalleryClick(e) {
+  e.preventDefault();
+  const currentImg = e.target.dataset.source;
+
+  if (!e.target.classList.contains("gallery__image")) {
+    return;
+  }
+
+  const instance = basicLightbox.create(
+    `
+ <img src="${currentImg}" width="800" height="600">
+ `,
+    {
+      onShow: (instance) => {
+        gallery.addEventListener("keydown", removeGalleryClick);
+      },
+      onClose: (instance) => {
+        gallery.removeEventListener("keydown", removeGalleryClick);
+      },
+    }
+  );
+  instance.show();
+
+  function removeGalleryClick(e) {
+    if (e.code === "Escape") {
+      instance.close();
+    }
+  }
+}
---
+++ js/02-lightbox.js
@@ -0,0 +1,23 @@
+import { galleryItems } from "./gallery-items.js";
+// Change code below this line
+
+let gallery = document.querySelector(".gallery");
+
+const markup = galleryItems
+  .map(
+    ({ preview, original, description }) =>
+      `<li class="gallery__item" >
+   <a class="gallery__link" href="${original}" >
+      <img class="gallery__image" src="${preview} "alt="${description}"/>   
+   </a>
+</li>`
+  )
+  .join("");
+
+gallery.insertAdjacentHTML("beforeend", markup);
+
+gallery = new SimpleLightbox(".gallery a", {
+  captionDelay: 250,
+  captionsData: "alt",
+  captionPosition: "bottom",
+});
